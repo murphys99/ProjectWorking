9d8462fb92245457861daf791eb962b132576b17
adamm <adamm@DESKTOP-B289VJJ>
2020-01-11 23:00:28 +0000

    Project Working now. Didn't try using product servlet yet

---
+++ src/java/com/bsapp/utils/IConstants.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.bsapp.utils;
+
+/**
+ *
+ * @author bemerson
+ */
+public interface IConstants {
+    
+    //User Types
+    public static final String USER_TYPE_ADMIN = "ADMIN";
+    public static final String USER_TYPE_GENERAL_USER = "GENUSER";
+    
+    
+    //Session Keys
+    public static final String SESSION_KEY_USER = "SKUSER";
+    public static final String SESSION_KEY_PRODUCT = "SKPRODUCT";
+    
+    public static final String REQUEST_KEY_ALL_USERS = "RKALLUSERS";
+    public static final String REQUEST_KEY_ALL_PRODUCTS = "RKALLPRODUCTS";
+}
